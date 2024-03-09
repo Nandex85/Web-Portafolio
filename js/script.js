d2a31b918646cfe527985f6dd8a438ee08633044
Nandex85 <chavezfer85@gmail.com>
2024-03-09 18:31:26 -0500

    primera parte cambiar de color la pagina

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function cambiarColor(){
+
+    var body = document.body;
+    /*var element = document.getElementById("miDiv");*/
+    body.style.backgroundColor = '#EEEEEE';
+    body.style.color = "#232931";
+}
